bf445299c76de559526573f4313031fcecdc9ea2
Ker Soon Kang <kang@gooddogdesign.com>
2024-09-22 16:25:23 +0930

    netlify functions update

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,51 +1,33 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
 const nodemailer = require("nodemailer");
-require('dotenv').config();
 
-// server used to send emails
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
+exports.handler = async (event) => {
+  const { firstName, lastName, email, message, phone } = JSON.parse(event.body);
+  
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.USERNAME,
-    pass: process.env.PASSWORD
-  },
-});
-
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
-});
-
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: "kskang033@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
+  const mailOptions = {
+    from: `${firstName} ${lastName}`,
+    to: "your_email@example.com",
+    subject: "Contact Form Submission",
+    html: `<p>Name: ${firstName} ${lastName}</p><p>Email: ${email}</p><p>Phone: ${phone}</p><p>Message: ${message}</p>`,
   };
-  contactEmail.sendMail(mail, (error, info) => {
-    if (error) {
-      console.error("Error sending email:", error);
-      res.status(500).json({ code: 500, status: "Error sending message", error: error.message });
-    } else {
-      res.status(200).json({ code: 200, status: "Message Sent", messageId: info.messageId });
-    }
-  });  
-});
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ status: "Message Sent" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ status: "Error" }),
+    };
+  }
+};
