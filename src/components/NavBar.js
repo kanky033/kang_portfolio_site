7b4cba7ffb8811d9bde70aef297122fdeb30c0d4
Ker Soon Kang <kang@gooddogdesign.com>
2024-10-17 15:16:34 +1030

    updated resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import linkedinLogo from '../assets/images/linkedin_logo.svg';
 import instagramLogo from '../assets/images/instagram_logo.svg';
 import githubLogo from '../assets/images/github_logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import resume from '../assets/downloadables/Kang_IT_Resume_Web.pdf';
+import resume from '../assets/downloadables/Kang_Resume.pdf';
 import DownloadButton from './DownloadButton'
 
 
